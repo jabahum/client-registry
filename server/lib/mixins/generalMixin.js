9048ed5f9451b0e8321a614827a17ca8a120665a
jabahum <jabajnan8@gmail.com>
2024-05-20 14:57:42 +0300

    add Unique Identifier sertup

--- server/lib/mixins/generalMixin.js
+++ server/lib/mixins/generalMixin.js
@@ -1,14 +1,30 @@
-'use strict';
+"use strict";
 /*global process, __dirname*/
-const fs = require('fs');
-const logger = require('../winston');
-const config = require('../config');
-const env = process.env.NODE_ENV || 'development';
+const fs = require("fs");
+const logger = require("../winston");
+const config = require("../config");
+const env = process.env.NODE_ENV || "development";
+
+const generatePatientUniqueIdentifier = () => {
+  let uniqueNumber = "";
+  if (uniqueNumber) {
+    uniqueNumber = "UG";
+  }
+
+  uniqueNumber = "";
+
+  return uniqueNumber;
+};
 
 const isMatchBroken = (resourceData, reference) => {
-  let isBroken = resourceData.extension && resourceData.extension.find((extension) => {
-    return extension.url === config.get("systems:brokenMatch:uri") && extension.valueReference.reference === reference;
-  });
+  let isBroken =
+    resourceData.extension &&
+    resourceData.extension.find((extension) => {
+      return (
+        extension.url === config.get("systems:brokenMatch:uri") &&
+        extension.valueReference.reference === reference
+      );
+    });
   return isBroken;
 };
 
@@ -20,14 +36,16 @@ const getClientDisplayName = (clientid) => {
   if (clientDet) {
     return clientDet.displayName;
   }
-  return '';
+  return "";
 };
 
 const getClientIdentifier = (resource) => {
   const internalIdURI = config.get("systems:internalid:uri");
-  const validSystem = resource.identifier && resource.identifier.find(identifier => {
-    return internalIdURI.includes(identifier.system) && identifier.value;
-  });
+  const validSystem =
+    resource.identifier &&
+    resource.identifier.find((identifier) => {
+      return internalIdURI.includes(identifier.system) && identifier.value;
+    });
   return validSystem;
 };
 
@@ -42,9 +60,9 @@ const setNestedKey = (obj, path, value, callback) => {
 };
 
 const updateConfigFile = (path, newValue, callback) => {
-  const pathString = path.join(':');
+  const pathString = path.join(":");
   config.set(pathString, newValue);
-  logger.info('Updating config file');
+  logger.info("Updating config file");
   const configFile = `${__dirname}/../../config/config_${env}.json`;
   const configData = require(configFile);
   setNestedKey(configData, path, newValue, () => {
@@ -52,18 +70,18 @@ const updateConfigFile = (path, newValue, callback) => {
       if (err) {
         throw err;
       }
-      logger.info('Done updating config file');
+      logger.info("Done updating config file");
       return callback();
     });
   });
 };
 
-const flattenComplex = extension => {
+const flattenComplex = (extension) => {
   let results = {};
   for (let ext of extension) {
-    let value = '';
+    let value = "";
     for (let key of Object.keys(ext)) {
-      if (key !== 'url') {
+      if (key !== "url") {
         value = ext[key];
       }
     }
@@ -84,12 +102,12 @@ const flattenComplex = extension => {
   return results;
 };
 
-const removeDir = function(path) {
+const removeDir = function (path) {
   if (fs.existsSync(path)) {
     const files = fs.readdirSync(path);
 
     if (files.length > 0) {
-      files.forEach(function(filename) {
+      files.forEach(function (filename) {
         if (fs.statSync(path + "/" + filename).isDirectory()) {
           removeDir(path + "/" + filename);
         } else {
@@ -104,10 +122,11 @@ const removeDir = function(path) {
 };
 
 module.exports = {
+  generatePatientUniqueIdentifier,
   updateConfigFile,
   flattenComplex,
   getClientIdentifier,
   getClientDisplayName,
   isMatchBroken,
-  removeDir
+  removeDir,
 };
