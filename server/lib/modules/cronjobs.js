054c37fac6836907b2922a223aac3f039e1642b0
jabahum <jabajnan8@gmail.com>
2024-06-14 22:03:57 +0300

    update docker

--- server/lib/modules/cronjobs.js
+++ server/lib/modules/cronjobs.js
@@ -5,17 +5,9 @@ const logger = require("../winston");
 
 let patientReprocessing = config.get("cronJobs:patientReprocessing");
 
-function isValidCronPattern(pattern) {
-  const cronRegex =
-    /^(\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*)$/;
-  return cronRegex.test(pattern);
-}
-
-if (isValidCronPattern) {
-  cron.schedule(patientReprocessing, () => {
+cron.schedule(patientReprocessing, () => {
     logger.info("Running cron job for patients reprocessing");
     matchMixin.reprocessPatients().then(() => {
       logger.info("Done running cron job for patients reprocessing");
     });
   });
-}
