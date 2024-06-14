524e7ebe6d85c041e9c38615f01aa13c18c4bbd4
jabahum <jabajnan8@gmail.com>
2024-06-14 16:01:03 +0300

    cron

--- server/lib/modules/cronjobs.js
+++ server/lib/modules/cronjobs.js
@@ -1,13 +1,21 @@
-const cron = require('node-cron');
-const matchMixin = require('../mixins/matchMixin');
-const config = require('../config');
-const logger = require('../winston');
+const cron = require("node-cron");
+const matchMixin = require("../mixins/matchMixin");
+const config = require("../config");
+const logger = require("../winston");
 
 let patientReprocessing = config.get("cronJobs:patientReprocessing");
 
-cron.schedule(patientReprocessing, () => {
-  logger.info('Running cron job for patients reprocessing');
-  matchMixin.reprocessPatients().then(() => {
-    logger.info('Done running cron job for patients reprocessing');
+function isValidCronPattern(pattern) {
+  const cronRegex =
+    /^(\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*) (\*|\d+|\d+\/\d+|\/\d+|\d+-\d+|\d+,\d+|\*)$/;
+  return cronRegex.test(pattern);
+}
+
+if (isValidCronPattern) {
+  cron.schedule(patientReprocessing, () => {
+    logger.info("Running cron job for patients reprocessing");
+    matchMixin.reprocessPatients().then(() => {
+      logger.info("Done running cron job for patients reprocessing");
+    });
   });
-});
+}
